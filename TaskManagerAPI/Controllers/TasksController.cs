8682c8eab48d185345333f93112132f1b85bb63f
MoKhatib2 <121944981+MoKhatib2@users.noreply.github.com>
2023-08-15 14:02:00 +0300

    Update TasksController.cs

--- TaskManagerAPI/Controllers/TasksController.cs
+++ TaskManagerAPI/Controllers/TasksController.cs
@@ -9,6 +9,7 @@ namespace TaskManagerAPI.Controllers
     public class TasksController : Controller
     {
         private readonly TaskManagerDBContext _taskManagerDBContext;
+        private int test2;
         public TasksController(TaskManagerDBContext taskManagerDBContext)
         {
             this._taskManagerDBContext = taskManagerDBContext;
